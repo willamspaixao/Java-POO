0a3dc2bb5a74c314ad275c9782393962609553da
willamspaixao <145800931+willamspaixao@users.noreply.github.com>
2024-04-02 21:17:14 -0300

    Update App.java
    ..

--- App.java
+++ App.java
@@ -1,23 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    Carro carro1 = new Carro();
-    carro1.registraAno(2004);
-    carro1.registraCor("branco");
-    carro1.registraModelo("Palio");
-    carro1.registraVelocidade(111);
-    /*
-    carro1.ligar();
-    carro1.desligar();
-    carro1.acelerar();
-    carro1.frear();
+    /*Carro carro1 = new Carro();
+    carro1.setAnoFabricacao(2024);
+    carro1.setCor("cinza");
+    carro1.setModelo("Jeep Renegade");
+    carro1.setVelocidade(110);
+    Carro carro2 = new Carro(2019,"branco","VW Virtus",145);
+    // carro1.ligar();
+    // carro1.desligar();
+    // carro1.acelerar();
+    // carro1.frear();
     carro1.mostrarDados();
-    System.out.println(carro1);
-*/
-    Conta conta1 = new Conta ();
-	Conta conta2 = new Conta ();
+    carro2.mostrarDados();
+   */
+    Conta conta1 = new Conta ("Willams", "123", 1000, 1500);
+    Conta conta2 = new Conta ();
 
-	conta1.inserirDados ("Willams", "123", 1000, 1500);
-	conta2.inserirDados ("Jessica", "456", 5000, 4500);
+    conta2.inserirDados ("Jessica", "456", 5000, 4500);
     
     conta1.creditarValor(5000);
     conta1.obterSaldo();
